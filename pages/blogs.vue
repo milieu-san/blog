826b5f820bad7fd46ff3f23a28d5c3b11ddae8df
miball0202 <aisacart@gmail.com>
2021-05-30 22:32:35 +0900

    Create Blogs Page

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-half">
+              <p class="title">
+                Coming Soon...
+              </p>
+              <p>
+                きっと きが むいたら しっぴつ するだろう
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
