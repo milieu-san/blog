c9f50f12c33ec6cced7fe9569dc49201c80406ba
miball0202 <aisacart@gmail.com>
2021-06-01 21:47:53 +0900

    Create Sapmle Blog Page

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="container">
+  <div class="container is-fluid">
     <div class="hero">
       <div class="hero-body">
         <div class="container">
-          <div class="columns is-centered">
+          <div class="columns is-left">
             <div class="column is-half">
               <p class="title">
-                Coming Soon...
+                Blogs
               </p>
               <p>
-                きっと きが むいたら しっぴつ するだろう
+                きっと きが むいたら こうしん するだろう
               </p>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column is-three-quarters">
+              <div class="nes-container with-title is-rounded mt-5 pb-0">
+                <p class="title">
+                  はじめてのブログです
+                </p>
+                <div class="columns">
+                  <p class="column is-three-quarters">
+                    こんにちは どんだけ あるいても こんなことに サンプル テキスト なのですよ...
+                  </p>
+                  <div class="column is-one-quarter has-text-centered">
+                    <i class="nes-jp-logo" />
+                  </div>
+                </div>
+                <div class="columns">
+                  <div class="column is-one-fifth py-0">
+                    <span>2021-05-03</span>
+                  </div>
+                  <div class="column py-0">
+                    <span>Life</span> / <span>Tech</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="column mt-5">
+              <div class="nes-field is-flex">
+                <input id="search_box" type="text" class="nes-input">
+                <span type="button" class="nes-btn is-primary">Search</span>
+              </div>
             </div>
           </div>
         </div>
