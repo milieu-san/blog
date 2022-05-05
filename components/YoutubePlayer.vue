7a6a848134799039bd85da68762141b3d36b92e9
miball0202 <aisacart@gmail.com>
2022-05-05 15:47:00 +0900

    Create youtube-player components. Replace all

---
+++ components/YoutubePlayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="url"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</template>
+
+<script>
+export default {
+  name: 'YoutubePlayer',
+  props: {
+    id: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  computed: {
+    url () {
+      return `https://www.youtube.com/embed/${this.id}`
+    }
+  }
+}
+</script>
